36906039305d08aabc4c5aa3c0d61690da3ed036
Igor Dugandzija <dugandzija@gmail.com>
2019-03-16 08:40:22 +0100

    final

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("myNavigation");
-    if (x.className === "nav__desktop") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav__desktop";
-    }
-  }
